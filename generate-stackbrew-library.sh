6d092760fd1ddc1599fec313b61f8561b7634c81
gerrit-photon <photon-checkins@vmware.com>
2018-07-09 19:23:13 +0000

    Fix photon docker file
    Change-Id: Ida251cb96f0525eee8f032ffbf24ff95dbd42709

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -12,32 +12,21 @@ fileCommit() {
 
 cat > photon << EOF
 # this file is generated via https://github.com/vmware/photon-docker-image/blob/$(fileCommit "$self")/$self
-
 Maintainers: Fabio Rapposelli <fabio@vmware.com> (@frapposelli),
              Alexey Makhalov <amakhalov@vmware.com> (@YustasSwamp)
 GitRepo: https://github.com/vmware/photon-docker-image.git
 Directory: docker
-
 EOF
 
 latestTag=", latest"
 
 for version in $versions ; do
-  extraTags=", $version$latestTag"
+  branchLine=$(git ls-remote --heads origin $version\* | sort -r -k2 | head -n1)
+  branch=${branchLine: -12}
+  commit=${branchLine:0:40}
+  echo >> photon
+  echo "Tags: $version, $branch$latestTag" >> photon
+  echo "GitFetch: refs/heads/$branch" >> photon
+  echo "GitCommit: $commit" >> photon
   latestTag=""
-  git ls-remote --heads origin $version\* | sort -r -k2 | while read branchLine ; do
-    branch=${branchLine: -12}
-    commit=${branchLine:0:40}
-    echo "Tags: $branch$extraTags" >> photon
-    echo "GitFetch: refs/heads/$branch" >> photon
-    echo "GitCommit: $commit" >> photon
-    echo >> photon
-    extraTags=""
-  done
 done
-
-cat >> photon << EOF
-Tags: 1.0GA
-GitFetch: refs/heads/dist
-GitCommit: 2043d10673cdb19ef656cbe0686e62345f4517b2
-EOF
