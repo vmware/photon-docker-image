5649464b97e3505c516be237d5ccc6bd2f476088
gerrit-photon <photon-checkins@vmware.com>
2019-03-09 00:00:09 +0000

    Update generate-stackbrew-library.sh to snap latest tag on 3.0 branch
    Change-Id: I9269641d02d5f5a6214a3f69ce15849d2e8ccefc

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 set -e
 
-versions="2.0 1.0"
-
 self="$(basename "$BASH_SOURCE")"
 
 # get the most recent commit which modified any of "$@"
 fileCommit() {
   git log -1 --format='format:%H' HEAD -- "$@"
+}
+
+only_x86_64_branch() {
+  version=$1
+  branchLine=$(git ls-remote --heads origin $version\* | sort -r -k2 | head -n1)
+  branch=${branchLine: -12}
+  commit=${branchLine:0:40}
+  echo >> photon
+  echo "Tags: $version, $branch$latestTag" >> photon
+  echo "GitFetch: refs/heads/$branch" >> photon
+  echo "GitCommit: $commit" >> photon
+  latestTag=""
 }
 
 aarch64_branch() {
@@ -21,15 +31,19 @@ aarch64_branch() {
   if [ -n "$x86_64branch" -a -n "$aarch64branch" ] ; then
     test "$x86_64branch" \> "$aarch64branch" && tag=$x86_64branch || tag=$aarch64branch
     echo >> photon
-    echo "Tags: $version, $tag" >> photon
+    echo "Tags: $version, $tag$latestTag" >> photon
     echo "Architectures: amd64, arm64v8" >> photon
     echo "GitFetch: refs/heads/x86_64/$x86_64branch" >> photon
     echo "GitCommit: $x86_64commit" >> photon
     echo "arm64v8-GitFetch: refs/heads/aarch64/$aarch64branch" >> photon
     echo "arm64v8-GitCommit: $aarch64commit" >> photon
+    latestTag=""
   fi
 }
 
+###################
+# start from here #
+###################
 cat > photon << EOF
 # this file is generated via https://github.com/vmware/photon-docker-image/blob/$(fileCommit "$self")/$self
 Maintainers: Fabio Rapposelli <fabio@vmware.com> (@frapposelli),
@@ -40,19 +54,16 @@ EOF
 
 latestTag=", latest"
 
-for version in $versions ; do
-  branchLine=$(git ls-remote --heads origin $version\* | sort -r -k2 | head -n1)
-  branch=${branchLine: -12}
-  commit=${branchLine:0:40}
-  echo >> photon
-  echo "Tags: $version, $branch$latestTag" >> photon
-  echo "GitFetch: refs/heads/$branch" >> photon
-  echo "GitCommit: $commit" >> photon
-  latestTag=""
-done
+# Note: For new release branches, please put it in the first one.
+# Then latest tag will be snapp at the new release branch
+# 3.0 branch
+aarch64_branch "3.0"
 
-# dev branch handling
+# dev branch
 aarch64_branch "dev"
 
-# 3.0- branch handling
-aarch64_branch "3.0"
+# 1.0 branch
+only_x86_64_branch "1.0"
+
+# 2.0 branch
+only_x86_64_branch "2.0"
